6f780996cf629aef83332c9d702ebc9d46b67afb
pongsatorn manusopit <ton11797@hotmail.com>
2017-11-19 22:28:00 +0700

    show

--- src/Show.java
+++ src/Show.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 /**
  * Created by Denice on 19/11/2560.
  */
-class costomerData{
+class customerData{
     private String name;
     private int seats;
-    public costomerData(String n,int s){
+    public customerData(String n,int s){
         name=n;
         seats=s;
     }
@@ -17,13 +17,25 @@ public class Show {
     private int day;
     private int time;
     private int seatLeft;
-    private ArrayList<costomerData> costomers;
+    private ArrayList<customerData> customers;
     public boolean bookSeats(String name,int seats){
         if(seatLeft>=seats){
-            costomers.add(new costomerData(name,seats));
+            customers.add(new customerData(name,seats));
             return true;
         }else{
             return false;
+        }
+    }
+
+    public void printlist(){
+        System.out.printf("Day %d",day);
+        if (time==1){
+            System.out.printf(" (afternoon) : ",day);
+        }else{
+            System.out.printf(" (evening) : ",day);
+        }
+        for(int i =0;i<customers.size();i++){
+            System.out.printf("%s (%d seats) ",customers.get(i).getName(),customers.get(i).getSeats());
         }
     }
 }
